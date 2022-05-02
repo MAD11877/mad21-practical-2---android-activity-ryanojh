e88a96661c7d47bb34afeb95354e28170d8baa2a
ryanooi <s10223647@connect.np.edu.sg>
2022-05-02 22:44:30 +0800

    Changed User class

--- app/src/main/java/sg/edu/np/mad/practical2/User.java
+++ app/src/main/java/sg/edu/np/mad/practical2/User.java
@@ -1,8 +1,43 @@
 package sg.edu.np.mad.practical2;
 
 public class User {
-    String Name;
-    String Description;
-    int Id;
-    boolean Followed;
+    private String name;
+    private String description;
+    private int id;
+    private boolean followed;
+
+    public User() {
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String d) {
+        description = d;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int i) {
+        id = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFollowed(boolean f) {
+        followed = f;
+    }
+
+    public boolean getFollowed() {
+        return followed;
+    }
 }
